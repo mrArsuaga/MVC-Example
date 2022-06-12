08fe0e26deda62469c5ee0e1faaee9c180422f63
Jose Arsuaga <jose.arsuaga@walmart.com>
2022-06-11 19:14:11 -0500

    Initial Commit

---
+++ MVC Example/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  MVC Example
+//
+//  Created by Jose Arsuaga on 11/06/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonTapped() {
+        
+    }
+
+
+}
+
