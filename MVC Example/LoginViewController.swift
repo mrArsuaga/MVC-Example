b674ead89099068e1bfada90d652a368c2938874
Jose Arsuaga <jose.arsuaga@walmart.com>
2022-06-11 19:37:30 -0500

    Add login to enable login button when both textfields have text

--- MVC Example/LoginViewController.swift
+++ MVC Example/LoginViewController.swift
@@ -11,10 +11,17 @@ class LoginViewController: UIViewController {
     
     @IBOutlet private weak var emailTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        emailTextField.addTarget(self, action: #selector(self.validateFields), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(self.validateFields), for: .editingChanged)
+    }
+    
+    @objc private func validateFields() {
+        loginButton.isEnabled = !(emailTextField.text?.isEmpty ?? false) && !(passwordTextField.text?.isEmpty ?? false)
     }
     
     @IBAction func loginButtonTapped() {
